0a8d04a44c7cbd77448b76cf5d8e9a9aff5bd06d
bracesproul <braceasproul@gmail.com>
2025-09-04 13:25:05 -0700

    chore: Add logs for vercel url

--- apps/web/src/lib/api-url.ts
+++ apps/web/src/lib/api-url.ts
@@ -12,10 +12,32 @@ export function getApiUrl() {
     ? `https://${process.env.VERCEL_URL}`
     : undefined;
   try {
+    console.log("vercelUrl", vercelUrl);
+    console.log("process.env.VERCEL_URL", process.env.VERCEL_URL);
+    console.log(
+      "process.env.VERCEL_PROJECT_PRODUCTION_URL",
+      process.env.VERCEL_PROJECT_PRODUCTION_URL,
+    );
+    console.log("process.env.VERCEL_BRANCH_URL", process.env.VERCEL_BRANCH_URL);
+
+    console.log(
+      "process.env.NEXT_PUBLIC_VERCEL_URL",
+      process.env.NEXT_PUBLIC_VERCEL_URL,
+    );
+    console.log(
+      "process.env.NEXT_PUBLIC_VERCEL_PROJECT_PRODUCTION_URL",
+      process.env.NEXT_PUBLIC_VERCEL_PROJECT_PRODUCTION_URL,
+    );
+    console.log(
+      "process.env.NEXT_PUBLIC_VERCEL_BRANCH_URL",
+      process.env.NEXT_PUBLIC_VERCEL_BRANCH_URL,
+    );
+
     const baseUrl = new URL(vercelUrl ?? fallbackLocalUrl);
     baseUrl.pathname = "/api";
     return baseUrl.toString();
-  } catch {
+  } catch (e) {
+    console.error("Failed to construct API URL", e);
     // Add a catch in case the NEXT_PUBLIC_VERCEL_URL is invalid and throws an error
     const baseUrl = new URL(fallbackLocalUrl);
     baseUrl.pathname = "/api";
